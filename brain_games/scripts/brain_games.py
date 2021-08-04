c4a492a519ef9a8ad3360f5cd8061ced2f58bc3a
Urtaev Stanislav <urtaev.s@gmail.com>
2021-08-04 23:48:40 +0300

    brain_games/scripts/brain_games.py 3rd step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python3
+
 def main():
     print('Welcome to the Brain Games!')
+
+
+#from ..cli import welcome_user
+from brain_games.cli import welcome_user
+
+def main():
+    welcome_user()
+    
 if __name__ == '__main__':
     main()
+
+
