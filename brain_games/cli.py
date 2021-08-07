3a611f93e232c125fa6d30195d9e994c5b7c3ab7
Urtaev Stanislav <urtaev.s@gmail.com>
2021-08-07 15:09:35 +0300

    clean up code

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,5 @@ import prompt
 
 
 def welcome_user():
-    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f"Hello, {name} !")
+    print('Hello, {}!'.format(name))
