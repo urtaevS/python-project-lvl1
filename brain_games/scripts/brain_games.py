047db0abbba8f50ddc9a2b99a41a03b0cf8994fa
Urtaev Stanislav <urtaev.s@gmail.com>
2021-08-07 15:10:06 +0300

    clean up code

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
-def main():
-    print('Welcome to the Brain Games!')
-
-
-#from ..cli import welcome_user
 from brain_games.cli import welcome_user
 
+
 def main():
+    print('Welcome to the Brain Games!')
     welcome_user()
-    
+
+
 if __name__ == '__main__':
     main()
-
-
