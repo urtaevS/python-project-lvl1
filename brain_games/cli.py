5d208dceabd55aab6a226343936750c33080cff5
Urtaev Stanislav <urtaev.s@gmail.com>
2021-08-08 14:54:57 +0300

    fix D103 Missing docstring in public function

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
 import prompt
 
 
 def welcome_user():
+    """Get an user name and promt user."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
