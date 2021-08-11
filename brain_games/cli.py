71aa6af238d76d812e47c87857ef4e54be7244cb
Urtaev Stanislav <urtaev.s@gmail.com>
2021-08-11 15:49:23 +0300

    func optimize

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,5 +3,9 @@ import prompt
 
 def welcome_user():
     """Get an user name and promt user."""
+    greeting()
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
+
+def greeting():
+    print('Welcome to the Brain Games!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,7 +3,6 @@ from brain_games.cli import welcome_user
 
 def main():
     """Greeting an user and print."""
-    print('Welcome to the Brain Games!')
     welcome_user()
 
 
