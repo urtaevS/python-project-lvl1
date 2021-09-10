057087d35911e601c12b43270da08660887155d9
Urtaev Stanislav <urtaev.s@gmail.com>
2021-09-10 10:28:48 +0300

    even.py updated

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,24 +1,9 @@
 import random
 
-from brain_games.games.variables import wrong_answer
-
-from brain_games.games.greeting import greeting
-
 
 def even():
-    """Guess the even number"""
-    name = greeting()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    count = 0
-    while count < 3:
-        num = random.randint(1, 100)
-        print('Question: {0}'.format(num))
-        answer = input(str('Your answer: '))
-        right_answer = 'yes' if num % 2 == 0 else 'no'
-        if answer == right_answer:
-            print('Correct!')
-            count += 1
-        else:
-            print(wrong_answer.format(answer, right_answer, name))
-            return
-    print('Congratulations, {0}!'.format(name))
+    num = random.randint(1, 100)
+    print('Question: {0}'.format(num))
+    right_answer = 'yes' if num % 2 == 0 else 'no'
+    answer = input(str('Your answer: '))
+    return right_answer, answer
--- brain_games/games/variables.py
+++ brain_games/games/variables.py
@@ -1,4 +1,10 @@
+
 wrong_answer = """'{0}' is wrong answer ;(.Correct answer was '{1}'.
 Let's try again, {2}!"""
 
+congrats = 'Congratulations, {0}!'
+
+
 welcome = 'Welcome to the Brain Games!'
+
+question = 'Answer "yes" if the number is even, otherwise answer "no".'
