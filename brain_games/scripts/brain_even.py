3e30814c9ca1ed831aad3f02964d34ab5cea40dc
Urtaev Stanislav <urtaev.s@gmail.com>
2021-09-11 14:54:49 +0300

    optimize even script

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,27 +1,4 @@
-from brain_games.games.even import even
-
-import prompt
-
-
-from brain_games.games.variables import wrong_answer, question
-
-
-def core():
-    """Get an user name and promt user."""
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
-    print(question)
-    count = 0
-    while count < 3:
-        right_answer, answer = even()
-        if answer == right_answer:
-            print('Correct!')
-            count += 1
-        else:
-            print(wrong_answer.format(answer, right_answer, name))
-            return
-    print('Congratulations, {0}!'.format(name))
+from brain_games.games.even import core
 
 
 def main():
