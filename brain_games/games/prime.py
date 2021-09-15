6fe4e1fa481202a991f2b37c70c0e37111ff7a42
Urtaev Stanislav <urtaev.s@gmail.com>
2021-09-15 12:13:45 +0300

    variable renamed

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,24 +4,24 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def round():
-    number1 = randint(1, 100)
-    question = number1
-    correct_answer = 'yes' if prime_num(number1) else 'no'
+    number = randint(1, 100)
+    question = number
+    correct_answer = 'yes' if prime_number(number) else 'no'
     return correct_answer, question
 
 
-def prime_num(number1):
+def prime_number(number):
     """Predicate False or True for def prime()"""
     div = 1
     count = 1
 
-    if number1 < 2 or number1 % 2 == 0 and number1 != 2:
+    if number < 2 or number % 2 == 0 and number != 2:
         return False
-    elif number1 == 2:
+    elif number == 2:
         return True
     else:
-        while div < number1 and count < 3:
-            result = number1 % div
+        while div < number and count < 3:
+            result = number % div
             div += 2
             if result == 0:
                 count += 1
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,18 +5,18 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 def round():
     replace_number = '..'
-    number1 = randint(1, 100)
+    number = randint(1, 100)
     step = randint(1, 5)
     string = ''
-    buffer_number = number1
-
+    buffer_number = number
     length_string = 0
+
     for length_string in range(10):
         string = string + str(buffer_number) + ' '
         last_number = buffer_number + step
         buffer_number = last_number
         length_string += 1
-    correct_answer = randrange(number1, last_number, step)
+    correct_answer = randrange(number, last_number, step)
     correct_answer = str(correct_answer)
     string_replace = string.replace(correct_answer, replace_number)
 
