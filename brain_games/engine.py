4d48fe93bc2c969f049570e6c3591889f813613a
Urtaev Stanislav <urtaev.s@gmail.com>
2021-09-15 17:53:16 +0300

    fix setup+lint

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -15,7 +15,8 @@ def run(round):
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if correct_answer != answer:
-            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.')
+            print(f'{answer} is wrong answer ;(.'
+            f'Correct answer was {correct_answer}.')
             print(f'Let\'s try again, {name}!')
             return
         else:
