c6e0787b43742a50b7b0b232e500576df54d9ccd
Urtaev Stanislav <urtaev.s@gmail.com>
2021-09-22 09:41:46 +0300

    rename variable

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -13,20 +13,16 @@ def generate_numbers():
 def get_question_and_correct_answer():
     number, step = generate_numbers()
 
-    replace_number = '..'
-    string = ''
+    row_of_numbers = ''
     buffer_number = number
-    length_string = 0
+    length_row_of_numbers = 0
 
-    for length_string in range(10):
-        string = string + str(buffer_number) + ' '
+    for length_row_of_numbers in range(10):
+        row_of_numbers = row_of_numbers + str(buffer_number) + ' '
         last_number = buffer_number + step
         buffer_number = last_number
-        length_string += 1
-    correct_answer = randrange(number, last_number, step)
-    correct_answer = str(correct_answer)
-    string_replace = string.replace(correct_answer, replace_number)
-
-    question = string_replace
+        length_row_of_numbers += 1
+    correct_answer = str(randrange(number, last_number, step))
+    question = row_of_numbers.replace(correct_answer, '..')
 
     return correct_answer, question
