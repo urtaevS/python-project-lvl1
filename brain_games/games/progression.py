58366e361d748e6bcd6e04f6eecc98ef5adcc393
Hexlet Learn <urtaev.s@gmail.com>
2021-10-12 10:19:17 +0300

    rename generate function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,15 +3,15 @@ from random import randint, randrange
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def generate_numbers():
+def generate_game_data():
     number = randint(1, 100)
     step = randint(1, 5)
 
     return number, step
 
 
-def generate_game_data():
-    number, step = generate_numbers()
+def get_question_and_correct_answer():
+    number, step = generate_game_data()
 
     row_of_numbers = ''
     buffer_number = number
