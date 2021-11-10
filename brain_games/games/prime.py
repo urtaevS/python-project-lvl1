ade082901b2b44e8584e544acc0e3209a363f814
Hexlet Learn <urtaev.s@gmail.com>
2021-11-10 18:23:35 +0300

    nothing change

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -17,11 +17,9 @@ def get_question_and_answer():
     """Collect 'question' and 'correct_answer' for games"""
 
     number = generate_game_data()
-    print(number)
     question = number
-    print(question)
     correct_answer = 'yes' if is_prime(number) is True else 'no'
-    print(correct_answer)
+
     return correct_answer, question
 
 
@@ -29,19 +27,19 @@ def is_prime(number):
     """Check variable 'number' prime or not"""
 
     divider_number = 1
-    divide_count = 1
+    divide_count = 0
 
     if number < 2 or number % 2 == 0 and number != 2:
         return False
     elif number == 2:
         return True
     else:
-        while divider_number < number and divide_count < 3:
+        while divider_number < number and divide_count < 2:
             result = number % divider_number
             divider_number += 2
             if result == 0:
                 divide_count += 1
-        if divide_count > 2:
+        if divide_count > 1:
             return False
         else:
             return True
